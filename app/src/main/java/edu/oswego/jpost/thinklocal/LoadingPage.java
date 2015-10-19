3c72467b5d0d4496ebacf299a37281410f2e853f
Joshua Post <Josh@Joshuas-MacBook-Air.local>
2015-10-19 15:12:41 -0400

    Initial commit

---
+++ app/src/main/java/edu/oswego/jpost/thinklocal/LoadingPage.java
@@ -0,0 +1,40 @@
+package edu.oswego.jpost.thinklocal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.logging.Handler;
+
+public class LoadingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally{
+                    Intent intent = new Intent(LoadingPage.this,ItemListActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
---
+++ app/src/main/java/edu/oswego/jpost/thinklocal/MainActivity.java
@@ -0,0 +1,48 @@
+package edu.oswego.jpost.thinklocal;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+        public static final String EXTRA_MESSAGE = "name";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            ListView listView = (ListView) findViewById(android.R.id.list);
+            if (listView == null) {
+                Log.d("Main", "listView == null");
+            }
+
+            String[] players = new String[] {
+                    "Homer", "Marge", "Bart", "Lisa", "Maggie"
+            };
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_list_item_1, android.R.id.text1,
+                    players);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    TextView clickedView = (TextView) view;
+                    Toast.makeText(MainActivity.this, "Item with id \"" + id + "\" " +
+                                    "& Position \"" + position + "\" " +
+                                    "& Name \"" + clickedView.getText() + "\" ",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
